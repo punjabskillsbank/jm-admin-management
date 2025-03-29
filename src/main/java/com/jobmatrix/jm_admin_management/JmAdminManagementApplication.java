7fd7a6500596ea1c8f884ffdb5be897b725b9eec
Ivkaran Singh <ivkaransingh@macbook-pro.mynetworksettings.com>
2025-03-29 05:25:27 -0400

    JMK_41: setup project

---
+++ src/main/java/com/jobmatrix/jm_admin_management/JmAdminManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jobmatrix.jm_admin_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmAdminManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmAdminManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobmatrix/jm_admin_management/JmAdminManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobmatrix.jm_admin_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmAdminManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
